3278e1d5b0f7ee66ec0287d3c209f57e427e9501
murtada1423 <murtada1423@gmail.com>
2026-07-22 02:44:07 +0300

    تراجع عن تعديل سرعة الفتح

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  poweredByHeader: false,
-  compress: true,
-  images: {
-    formats: ['image/avif', 'image/webp'],
-  },
-}
-export default nextConfig
+const nextConfig = {};
+export default nextConfig;
